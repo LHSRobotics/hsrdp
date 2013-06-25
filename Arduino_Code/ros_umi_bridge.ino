f3e07d00852bb8de27d8065363168e29f6c5ab61
mentarus <mentarus@gmail.com>
2013-06-25 01:23:27 +0100

    Converted to proper use  pin change interrupts

--- Arduino_Code/ros_umi_bridge.ino
+++ Arduino_Code/ros_umi_bridge.ino
@@ -1,20 +1,25 @@
-//#define DEBUG 1 //Debug toggle, uncomment line to enable
+#define UMI_DEBUG 1 // toggle, uncomment line to enable
+#define NO_PORTB_PINCHANGES // to indicate that port b will not be used for pin change interrupts (faster)
+#define NO_PORTJ_PINCHANGES // to indicate that port J will not be used for pin change interrupts (faster)
+//#define NO_PORTK_PINCHANGES // to indicate that port d will not be used for pin change interrupts (faster)
+
 
 #include <ros.h>
 #include <std_msgs/Int16MultiArray.h>
 #include "Streaming.h"
+#include "PinChangeInt.h"
 #include "Encoder.h"
 #include "MotorController.h"
 
 
 Encoder encoders[7] = {
-  Encoder(22,23),//gripper
-  Encoder(24,25),//wrist1
-  Encoder(26,27),//wrist2
-  Encoder(28,29),//wrist yaw
-  Encoder(30,31),//elbow
-  Encoder(32,33),//shoulder
-  Encoder(34,35)//Z-axis
+  Encoder(A8,23),//gripper
+  Encoder(A9,25),//wrist1
+  Encoder(A10,27),//wrist2
+  Encoder(A11,29),//wrist yaw
+  Encoder(A12,31),//elbow
+  Encoder(A13,33),//shoulder
+  Encoder(A14,35)//Z-axis
 };
 
 MotorController motors[7] = {
@@ -56,36 +61,32 @@ void setup() {
     nh.initNode();
     nh.advertise(pub_joints_state);
     nh.subscribe(sub);
-  
-    //This is where we connect our encoders, corresponds to encoders[7]
-    attachInterrupt(22, doEncoderGripper, CHANGE);
-    attachInterrupt(24, doEncoderWrist1, CHANGE);
-    attachInterrupt(26, doEncoderWrist2, CHANGE); 
-    attachInterrupt(28, doEncoderWristYaw, CHANGE);
-    attachInterrupt(30, doEncoderElbow, CHANGE);
-    attachInterrupt(32, doEncoderShoulder, CHANGE);
-    attachInterrupt(34, doEncoderZ, CHANGE);
-
-    pinMode(13, OUTPUT);
-       
-    #ifdef DEBUG
+     
+    #ifdef UMI_DEBUG
     Serial.begin (57600);
     Serial << "Start" << endl;
     #endif
+    
+    //This is where we connect our encoders, corresponds to encoders[7]
+    PCintPort::attachInterrupt(A8, doEncoderGripper, CHANGE);
+    PCintPort::attachInterrupt(A9, doEncoderWrist1, CHANGE);
+    PCintPort::attachInterrupt(A10, doEncoderWrist2, CHANGE); 
+    PCintPort::attachInterrupt(A11, doEncoderWristYaw, CHANGE);
+    PCintPort::attachInterrupt(A12, doEncoderElbow, CHANGE);
+    PCintPort::attachInterrupt(A13, doEncoderShoulder, CHANGE);
+    //attachInterrupt(0, doEncoderShoulder, CHANGE);
+    PCintPort::attachInterrupt(A14, doEncoderZ, CHANGE);
+    
+    pinMode(13, OUTPUT);
+       
+
 } 
 
 void loop(){
   if (millis() > publisher_timer) {
-    publisher_timer = millis() + 20; //publish at 50 hz
+    publisher_timer = millis() + 200; //publish at 5 hz
       
-      //Heartbeat
-      if(led_on){
-       digitalWrite(led, HIGH);
-       led_on = 0;
-      }else{
-       digitalWrite(led, LOW);
-       led_on = 1;
-      }
+
       joints_msg.data_length = 7;
       joints_msg.data = readEncoders();
       pub_joints_state.publish(&joints_msg);
@@ -122,6 +123,18 @@ void doEncoderElbow(){
 }
 
 void doEncoderShoulder(){
+    //Heartbeat
+    if(led_on){
+     digitalWrite(led, HIGH);
+     led_on = 0;
+    }else{
+     digitalWrite(led, LOW);
+     led_on = 1;
+    }
+  
+    #ifdef UMI_DEBUG
+    Serial << "Shoulder moved" << endl;
+    #endif
     processJoint(5);
 }
 
@@ -131,18 +144,18 @@ void doEncoderZ(){
 
 
 int* readEncoders(){
-    #ifdef DEBUG
-    Serial << "encoder readings: ";
+    #ifdef UMI_DEBUG
+    //Serial << "encoder readings: ";
     #endif
     for(int i=0; i < 7; i++)
     {
        positions[i] = encoders[i].getPosition();
-       #ifdef DEBUG
-       Serial << i << ":" << encoders[0].getPosition() << " ";
+       #ifdef UMI_DEBUG
+       //Serial << i << ":" << encoders[0].getPosition() << " ";
        #endif
     }
-       #ifdef DEBUG
-       Serial << endl;
+       #ifdef UMI_DEBUG
+       //Serial << endl;
        #endif
     return positions;
 }
